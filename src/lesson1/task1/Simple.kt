79a995be436e347857ad7fcf74768a67395189bf
koma-4 <koma_ni@mail.ru>
2020-09-10 09:14:15 +0300

    Решены задачи из первого урока

--- src/lesson1/task1/Simple.kt
+++ src/lesson1/task1/Simple.kt
@@ -65,7 +65,11 @@ fun main() {
  * Пользователь задает время в часах, минутах и секундах, например, 8:20:35.
  * Рассчитать время в секундах, прошедшее с начала суток (30035 в данном случае).
  */
-fun seconds(hours: Int, minutes: Int, seconds: Int): Int = TODO()
+fun seconds(hours: Int, minutes: Int, seconds: Int): Int {
+    val t = (hours * 3600 + minutes * 60 + seconds)
+    return t
+}
+
 
 /**
  * Тривиальная (1 балл)
@@ -74,7 +78,10 @@ fun seconds(hours: Int, minutes: Int, seconds: Int): Int = TODO()
  * Определить длину того же отрезка в метрах (в данном случае 18.98).
  * 1 сажень = 3 аршина = 48 вершков, 1 вершок = 4.445 см.
  */
-fun lengthInMeters(sagenes: Int, arshins: Int, vershoks: Int): Double = TODO()
+fun lengthInMeters(sagenes: Int, arshins: Int, vershoks: Int): Double {
+    val l = ((((sagenes * 3.0 + arshins)) * 16.0 + vershoks) * 4.445) / 100.0
+    return l
+}
 
 /**
  * Тривиальная (1 балл)
@@ -82,7 +89,10 @@ fun lengthInMeters(sagenes: Int, arshins: Int, vershoks: Int): Double = TODO()
  * Пользователь задает угол в градусах, минутах и секундах (например, 36 градусов 14 минут 35 секунд).
  * Вывести значение того же угла в радианах (например, 0.63256).
  */
-fun angleInRadian(deg: Int, min: Int, sec: Int): Double = TODO()
+fun angleInRadian(deg: Int, min: Int, sec: Int): Double {
+    val rad = (deg + min / 60.0 + sec / 3600.0) * PI / 180.0
+    return rad
+}
 
 /**
  * Тривиальная (1 балл)
@@ -90,7 +100,13 @@ fun angleInRadian(deg: Int, min: Int, sec: Int): Double = TODO()
  * Найти длину отрезка, соединяющего точки на плоскости с координатами (x1, y1) и (x2, y2).
  * Например, расстояние между (3, 0) и (0, 4) равно 5
  */
-fun trackLength(x1: Double, y1: Double, x2: Double, y2: Double): Double = TODO()
+fun trackLength(x1: Double, y1: Double, x2: Double, y2: Double): Double {
+    val a = x2 - x1
+    val b = y2 - y1
+    val c = sqr(a) + sqr(b)
+    val l = sqrt(c)
+    return l
+}
 
 /**
  * Простая (2 балла)
@@ -98,7 +114,10 @@ fun trackLength(x1: Double, y1: Double, x2: Double, y2: Double): Double = TODO()
  * Пользователь задает целое число, большее 100 (например, 3801).
  * Определить третью цифру справа в этом числе (в данном случае 8).
  */
-fun thirdDigit(number: Int): Int = TODO()
+fun thirdDigit(number: Int): Int {
+    val n = (number % 1000) / 100
+    return n
+}
 
 /**
  * Простая (2 балла)
@@ -107,7 +126,10 @@ fun thirdDigit(number: Int): Int = TODO()
  * прибыл на станцию назначения в h2 часов m2 минут того же дня (например в 13:01).
  * Определите время поезда в пути в минутах (в данном случае 216).
  */
-fun travelMinutes(hoursDepart: Int, minutesDepart: Int, hoursArrive: Int, minutesArrive: Int): Int = TODO()
+fun travelMinutes(hoursDepart: Int, minutesDepart: Int, hoursArrive: Int, minutesArrive: Int): Int {
+    val t = 60 - minutesDepart + minutesArrive + (hoursArrive- hoursDepart -1) * 60
+    return t
+}
 
 /**
  * Простая (2 балла)
@@ -116,7 +138,10 @@ fun travelMinutes(hoursDepart: Int, minutesDepart: Int, hoursArrive: Int, minute
  * Сколько денег будет на счету через 3 года (с учётом сложных процентов)?
  * Например, 100 рублей под 10% годовых превратятся в 133.1 рубля
  */
-fun accountInThreeYears(initial: Int, percent: Int): Double = TODO()
+fun accountInThreeYears(initial: Int, percent: Int): Double {
+    val a = initial * (1.0 + percent/100.0) * (1.0 + percent/100.0) * (1.0 + percent/100.0)
+    return a
+}
 
 /**
  * Простая (2 балла)
@@ -124,4 +149,10 @@ fun accountInThreeYears(initial: Int, percent: Int): Double = TODO()
  * Пользователь задает целое трехзначное число (например, 478).
  * Необходимо вывести число, полученное из заданного перестановкой цифр в обратном порядке (например, 874).
  */
-fun numberRevert(number: Int): Int = TODO()
+fun numberRevert(number: Int): Int {
+    val n = (number % 10) * 100
+    val e = ( (number/10) % 10) * 10
+    val w = ( number / 100)
+    val new = n + e + w
+    return new
+}
