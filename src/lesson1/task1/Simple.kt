1fb913a46b1ec5894cc4744f3112419e42bc84f5
koma-4 <koma_ni@mail.ru>
2020-09-11 11:08:35 +0300

    Исправлены ошибки

--- src/lesson1/task1/Simple.kt
+++ src/lesson1/task1/Simple.kt
@@ -65,7 +65,8 @@ fun main() {
  * Пользователь задает время в часах, минутах и секундах, например, 8:20:35.
  * Рассчитать время в секундах, прошедшее с начала суток (30035 в данном случае).
  */
-fun seconds(hours: Int, minutes: Int, seconds: Int): Int = (hours * 3600 + minutes * 60 + seconds)
+fun seconds(hours: Int, minutes: Int, seconds: Int) =
+    (hours * 3600 + minutes * 60 + seconds)
 
 
 
@@ -76,7 +77,8 @@ fun seconds(hours: Int, minutes: Int, seconds: Int): Int = (hours * 3600 + minut
  * Определить длину того же отрезка в метрах (в данном случае 18.98).
  * 1 сажень = 3 аршина = 48 вершков, 1 вершок = 4.445 см.
  */
-fun lengthInMeters(sagenes: Int, arshins: Int, vershoks: Int): Double = ((((sagenes * 3.0 + arshins)) * 16.0 + vershoks) * 4.445) / 100.0
+fun lengthInMeters(sagenes: Int, arshins: Int, vershoks: Int) =
+    ((((sagenes * 3.0 + arshins)) * 16.0 + vershoks) * 4.445) / 100.0
 
 /**
  * Тривиальная (1 балл)
@@ -84,7 +86,8 @@ fun lengthInMeters(sagenes: Int, arshins: Int, vershoks: Int): Double = ((((sage
  * Пользователь задает угол в градусах, минутах и секундах (например, 36 градусов 14 минут 35 секунд).
  * Вывести значение того же угла в радианах (например, 0.63256).
  */
-fun angleInRadian(deg: Int, min: Int, sec: Int): Double = (deg + min / 60.0 + sec / 3600.0) * PI / 180.0
+fun angleInRadian(deg: Int, min: Int, sec: Int) =
+    (deg + min / 60.0 + sec / 3600.0) * PI / 180.0
 
 
 /**
@@ -107,7 +110,7 @@ fun trackLength(x1: Double, y1: Double, x2: Double, y2: Double): Double {
  * Пользователь задает целое число, большее 100 (например, 3801).
  * Определить третью цифру справа в этом числе (в данном случае 8).
  */
-fun thirdDigit(number: Int): Int = (number % 1000) / 100
+fun thirdDigit(number: Int) = (number % 1000) / 100
 
 /**
  * Простая (2 балла)
@@ -116,7 +119,8 @@ fun thirdDigit(number: Int): Int = (number % 1000) / 100
  * прибыл на станцию назначения в h2 часов m2 минут того же дня (например в 13:01).
  * Определите время поезда в пути в минутах (в данном случае 216).
  */
-fun travelMinutes(hoursDepart: Int, minutesDepart: Int, hoursArrive: Int, minutesArrive: Int): Int = 60 - minutesDepart + minutesArrive + (hoursArrive - hoursDepart - 1) * 60
+fun travelMinutes(hoursDepart: Int, minutesDepart: Int, hoursArrive: Int, minutesArrive: Int) =
+    60 - minutesDepart + minutesArrive + (hoursArrive - hoursDepart - 1) * 60
 
 /**
  * Простая (2 балла)
@@ -125,7 +129,8 @@ fun travelMinutes(hoursDepart: Int, minutesDepart: Int, hoursArrive: Int, minute
  * Сколько денег будет на счету через 3 года (с учётом сложных процентов)?
  * Например, 100 рублей под 10% годовых превратятся в 133.1 рубля
  */
-fun accountInThreeYears(initial: Int, percent: Int): Double = initial * (1.0 + percent / 100.0) * (1.0 + percent / 100.0) * (1.0 + percent / 100.0)
+fun accountInThreeYears(initial: Int, percent: Int) =
+    initial * (1.0 + percent / 100.0) * (1.0 + percent / 100.0) * (1.0 + percent / 100.0)
 
 /**
  * Простая (2 балла)
